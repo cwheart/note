82e9f2b76965a88c693ef1d2fdbd4e2168fc16e1
xin <xinshuaifeng@126.com>
2013-10-24 14:32:46 +0800

    puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :scm, :git
 # set :log_level, :debug
 # set :pty, true
 
-set :linked_files, %w{config/database.yml config/unicorn.rb}
+set :linked_files, %w{config/database.yml config/unicorn.rb config/puma.rb}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
